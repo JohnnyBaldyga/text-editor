845b0f21dfce3535bd0357b57543012450322a1d
JohnnyBaldyga <jonathanbaldyga@gmail.com>
2024-08-04 20:12:57 -0400

    but install event listener

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -8,8 +8,16 @@ window.addEventListener("beforeinstallprompt", (event) => {
   butInstall.style.display = "block";
 
   // TODO: Implement a click event handler on the `butInstall` element
-  butInstall.addEventListener("click", async () => {});
-
-  // TODO: Add an handler for the `appinstalled` event
-  window.addEventListener("appinstalled", (event) => {});
+  butInstall.addEventListener("click", async () => {
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+      return;
+    }
+    promptEvent.prompt();
+    window.deferredPrompt = null;
+    butInstall.style.display = "none";
+  });
 });
+
+// TODO: Add an handler for the `appinstalled` event
+window.addEventListener("appinstalled", (event) => {});
